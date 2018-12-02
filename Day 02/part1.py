719cda25e157dd84c817cbf252affcae7c6d70d0
EstevesAndre <up201606673@fe.up.pt>
2018-12-02 14:47:56 +0000

    commented part1

--- Day 02/part1.py
+++ Day 02/part1.py
@@ -2,22 +2,29 @@ file=open("input.in","r")
 
 content = file.readlines()
 file.close()
-content = [i.strip() for i in content]
 
-sum2 = 0
-sum3 = 0
+# \n erased
+content = [i.strip() for i in content] 
+
+
+countOf2 = 0
+countOf3 = 0
 
 for line in content:
+    # initializing variables to increment per line if found
     inc2 = 1
     inc3 = 1
-    while line != '':
+    while line != '' and not(inc2 == 0 and inc3 == 0):
         count = line.count(line[0])
         if count == 2:
-            sum2 += inc2
+            # line contains two line[0]
+            countOf2 += inc2 
             inc2 = 0
         elif count == 3:
-            sum3 += inc3
+            # line contains three line[0]
+            countOf3 += inc3
             inc3 = 0
+        # erases all chars (line[0]) of the line
         line = line.replace(line[0],'')
 
-print(sum2 * sum3)    
+print(countOf2 * countOf3)
