89a17b48d7a6c86a22adf08bacc1ea7e48b852af
EstevesAndre <up201606673@fe.up.pt>
2018-12-03 16:30:52 +0000

    added input and part1 completed!

---
+++ Day 03/part1.py
@@ -0,0 +1,26 @@
+import re
+
+file=open("input.in","r")
+
+content=file.readlines()
+content=[i.strip() for i in content]
+
+Matrix = [[0 for x in range(1000)] for y in range(1000)]
+
+for line in content:
+    line = list(map(int,re.findall(r'-?\d+', line)))    
+    for x in range(line[1], line[1]+line[3]):
+        for y in range(line[2], line[2]+line[4]):
+            if Matrix[x][y] == 0:
+                Matrix[x][y] = line[0]
+            elif Matrix[x][y] != -1:
+                Matrix[x][y] = -1
+
+sum = 0
+
+for x in range(1000):
+    for y in range(1000):
+        if Matrix[x][y] == -1:
+            sum += 1
+
+print(sum)
