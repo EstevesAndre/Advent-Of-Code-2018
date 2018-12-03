729b26e09dc4847647b0e5a19e438e275b4e9ec6
EstevesAndre <up201606673@fe.up.pt>
2018-12-03 16:57:50 +0000

    part2 completed!

---
+++ Day 03/part2.py
@@ -0,0 +1,28 @@
+import re
+
+file=open("input.in","r")
+
+content=file.readlines()
+content=[list(map(int,re.findall(r'-?\d+', line))) for line in content]
+
+Matrix = [[0 for x in range(1000)] for y in range(1000)]
+
+for line in content: 
+    for x in range(line[1], line[1]+line[3]):
+        for y in range(line[2], line[2]+line[4]):
+            if Matrix[x][y] == 0:
+                Matrix[x][y] = line[0]
+            elif Matrix[x][y] != -1:
+                Matrix[x][y] = -1
+
+def notOverlaped(line):
+    for x in range(line[1], line[1]+line[3]):
+        for y in range(line[2], line[2]+line[4]):
+            if Matrix[x][y] != line[0]:
+                return False
+    return True
+
+for line in content:
+    if notOverlaped(line):
+        print(line[0])
+        quit()
